a01e6e5ab1f3e9f32a58deafc5dd5f2d35c93d66
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-10-02 13:18:51 +1000

    Inject a component into MainActivity
    - Create somethink like the SomeText that we want to inject
    - Add dagger 2 libraries
    - Create a dagger inject component with fun inject

--- app/src/main/java/com/example/meeravasudevan/dagger2/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/dagger2/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.meeravasudevan.dagger2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import dagger.Component
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var someText: SomeText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DaggerInjectTextComponent.create().inject(this)
+        fooText.text = someText.text
     }
+}
+
+class SomeText @Inject constructor() {
+    var text = "Dagger 2 Experiment"
+}
+
+@Component
+interface InjectTextComponent {
+    fun inject(app: MainActivity)
 }

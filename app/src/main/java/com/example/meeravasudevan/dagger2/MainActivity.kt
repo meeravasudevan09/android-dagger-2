f766c56368f6664257345ec4811196454ea78822
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-10-02 14:53:30 +1000

    Introducing Provides wrapped in modules

--- app/src/main/java/com/example/meeravasudevan/dagger2/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/dagger2/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.meeravasudevan.dagger2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import dagger.Component
+import dagger.Module
+import dagger.Provides
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject lateinit var someText: SomeText
+    @Inject
+    lateinit var someText: SomeText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +22,17 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class SomeText @Inject constructor() {
-    var text = "Dagger 2 Experiment"
+@Module
+class TextModule {
+    @Provides
+    fun providesSomeText(): SomeText {
+        return SomeText("Learning Dagger Provides")
+    }
 }
 
-@Component
+class SomeText(val text: String)
+
+@Component(modules = [TextModule::class])
 interface InjectTextComponent {
     fun inject(app: MainActivity)
 }
